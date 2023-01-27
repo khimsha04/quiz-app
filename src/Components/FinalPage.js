a1ad0e5c3b5f5efb43414225c8dc8b4205ee88d5
Zurab Khimshiashvili <64226929+khimsha04@users.noreply.github.com>
2023-01-27 06:24:05 +0400

    second commit

--- src/Components/FinalPage.js
+++ src/Components/FinalPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from './Card';
 import "./FinalPage.css"
+import Images from "./Images/one.png"
 const FinalPage = ({
   score,
   setShowFinalPage,
@@ -21,8 +22,34 @@ const FinalPage = ({
     setShowStartingPage(true);
     setScore(0);
     setUsername("");
-  };
-
+  }; if(score<500){
+      return(
+        <div>
+          <div><img style={{width:400,height:300,margin:'auto'}} src={require('./Images/two.jpg')} alt='image'/></div>
+          <span style={{
+          fontSize:40,color:'black',borderRadius:10,position:'absolute',top:400,left:580
+         }}>ჩაიხედე წიგნებში</span>
+          <button className="play_again_btn" onClick={handleClick}>
+            თავიდან ცდა
+          </button>
+        </div>
+      )
+    }
+    if(score > 500){
+      return(
+        <div>
+          <div><img style={{width:400,height:300,margin:'auto'}} src={require('./Images/one.png')} alt='image'/></div>
+         <span style={{
+          fontSize:30,backgroundColor:'black',color:'white',borderRadius:10,position:'absolute',top:480,left:685
+         }}> გილოცავ</span>
+         <button className="play_again_btn" onClick={handleClick}>
+      თავიდან ცდა
+    </button>
+        </div>
+      )
+    }
+   
+  
   return (
   <Card>
     <h1 className="heading">თამაში დამთავრდა, {username}!</h1>
